410ada3ac63182a3b373912ffb0e49bd1d412793
sophiamoos1 <90751245+sophiamoos1@users.noreply.github.com>
2023-01-23 15:21:00 +0100

    added comments with java docs

--- K08/src/shippingObject/ShippingObject.java
+++ K08/src/shippingObject/ShippingObject.java
@@ -1,6 +1,63 @@
 package shippingObject;
 
+import person.Person;
+
 public abstract class ShippingObject {
     private String objectId;
+    private Person sendPerson;
+    private Person deliverPerson;
 
+    /**
+     * @param objectId
+     * @param sendPerson
+     * @param deliverPerson
+     */
+    public ShippingObject(String objectId, Person sendPerson, Person deliverPerson) {
+        this.objectId = objectId;
+        this.sendPerson = sendPerson;
+        this.deliverPerson = deliverPerson;
+    }
+
+
+    /**
+     * @return
+     */
+    public String getObjectId() {
+        return objectId;
+    }
+
+    /**
+     * @param objectId
+     */
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
+    /**
+     * @return
+     */
+    public Person getSendPerson() {
+        return sendPerson;
+    }
+
+    /**
+     * @param sendPerson
+     */
+    public void setSendPerson(Person sendPerson) {
+        this.sendPerson = sendPerson;
+    }
+
+    /**
+     * @return
+     */
+    public Person getDeliverPerson() {
+        return deliverPerson;
+    }
+
+    /**
+     * @param deliverPerson
+     */
+    public void setDeliverPerson(Person deliverPerson) {
+        this.deliverPerson = deliverPerson;
+    }
 }
