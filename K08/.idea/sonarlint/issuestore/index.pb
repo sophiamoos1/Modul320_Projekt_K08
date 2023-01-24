25150a676c840a3ae055021ef5ab65e1e88f8b56
sophiamoos1 <90751245+sophiamoos1@users.noreply.github.com>
2023-01-24 11:34:33 +0100

    added controller class

--- K08/.idea/sonarlint/issuestore/index.pb
+++ K08/.idea/sonarlint/issuestore/index.pb
@@ -3,5 +3,3 @@ V
 &src/packageStation/PackageStation.java,1\e\1e5501a8c056d9119e5477cb9dc1e71f8684b319
 L
 src/packageStation/Volg.java,9\a\9a91f10855280271d539648e64af8de25f60b099
-N
-src/shippingObject/Letter.java,b\3\b3443d64d566cc37fd9984885a29d63740a51909
--- K08/src/Main.java
+++ K08/src/Main.java
@@ -3,6 +3,6 @@ import services.Storage;
 public class Main {
     public static void main(String[] args) {
         Storage storage = new Storage();
-        System.out.println();
+        System.out.println(storage.getFirstPackage().getDeliverPerson().getAdress().returnFormattedAdress());
     }
 }
