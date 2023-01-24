0d3e35477b0dabfea5074e6dab33d1cf242a8e6e
sophiamoos1 <90751245+sophiamoos1@users.noreply.github.com>
2023-01-24 08:43:45 +0100

    implementet interface in post class

--- K08/src/packageStation/PackageStation.java
+++ K08/src/packageStation/PackageStation.java
@@ -5,11 +5,29 @@ import adress.Adress;
 import java.util.Date;
 
 public interface PackageStation {
+    /**
+     * @return
+     */
     Date arivalDate();
+
+    /**
+     * @return
+     */
     Adress adress();
 
+    /**
+     * @return
+     */
     String stationName();
 
+    /**
+     * @return
+     */
     Boolean lastStation();
+
+    /**
+     * @return
+     */
+    Boolean gotPickedUp();
 
 }
--- K08/src/packageStation/Post.java
+++ K08/src/packageStation/Post.java
@@ -1,6 +1,85 @@
 package packageStation;
 
+import adress.Adress;
+
+import java.util.Date;
+
 public class Post implements PackageStation{
-    private Boolean pick;
-    private
+    private Date arrivalDate;
+    private Adress address;
+    private String name;
+    private String postStation;
+    private Boolean isLastStation;
+    private Boolean gotPickedUp;
+
+    /**
+     * @param arivalDate
+     * @param address
+     * @param stationName
+     * @param postStation
+     * @param lastStation
+     * @param gotPickedUp
+     */
+    public Post(Date arivalDate, Adress address, String stationName, String postStation, Boolean lastStation, Boolean gotPickedUp) {
+        this.arrivalDate = arivalDate;
+        this.address = address;
+        this.name = stationName;
+        this.postStation = postStation;
+        this.isLastStation = lastStation;
+        this.gotPickedUp = gotPickedUp;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public Date arivalDate() {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public Adress adress() {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String stationName() {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public Boolean lastStation() {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public Boolean gotPickedUp() {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public String getPostStation() {
+        return postStation;
+    }
+
+    /**
+     * @param postStation
+     */
+    public void setPostStation(String postStation) {
+        this.postStation = postStation;
+    }
 }
